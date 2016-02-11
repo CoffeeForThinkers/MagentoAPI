df6a1a31b37512f47ce87d731dfcd3cb6f12ae16
Dustin Oprea <myselfasunder@gmail.com>
2016-02-11 02:39:34 -0500

    Added attribute-set distillation.

--- ma/api/catalog_product_attribute_set.py
+++ ma/api/catalog_product_attribute_set.py
@@ -8,4 +8,7 @@ _LOGGER = logging.getLogger()
 class CatalogProductAttributeSetApi(ma.api.base_class.Api):
     def get_list(self):
         l = self.magento.catalog_product_attribute_set.list()
-        return l
+        for record in l:
+            record['set_id'] = int(record['set_id'])
+
+            yield record
