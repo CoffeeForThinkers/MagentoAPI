a2943a23be840c242672c5d4fb3b84c98cbffc8d
Dustin Oprea <myselfasunder@gmail.com>
2016-02-15 01:31:53 -0500

    Added get_dict_from_named_tuple.

--- ma/utility.py
+++ ma/utility.py
@@ -6,3 +6,16 @@ def pretty_print(results):
             sort_keys=True,
             indent=4, 
             separators=(',', ': ')))
+
+def get_dict_from_named_tuple(nt):
+    a = dir(nt)
+    d = { 
+            k: getattr(nt, k) 
+            for k 
+            in a 
+            if k[0] != '_' \
+                and k != 'count' \
+                and k != 'index' 
+        }
+
+    return d
