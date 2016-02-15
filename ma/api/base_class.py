07c92a4a29c278e6a6f3123c641ec9b5d18b3034
Dustin Oprea <myselfasunder@gmail.com>
2016-02-15 01:32:25 -0500

    We now take the scheme from the environment.

--- ma/api/base_class.py
+++ ma/api/base_class.py
@@ -10,33 +10,56 @@ _TIMESTAMP_FORMAT = '%Y-%m-%d %H:%M:%S'
 _LOGGER = logging.getLogger(__name__)
 
 
-class Api(object):
+class _ApiResources(object):
     def __init__(self):
-        self.__m = self.__get_resource()
+        self.__ar = self.__get_api_resource()
 
     def __get_server_config(self):
+        scheme = ma.config.magento.SCHEME
         username = ma.config.magento.USERNAME
         password = ma.config.magento.PASSWORD
         hostname = ma.config.magento.HOSTNAME
         port = ma.config.magento.PORT
 
         return {
+            'scheme': scheme,
             'username': username,
             'password': password,
             'hostname': hostname,
             'port': port,
         }
 
-    def __get_resource(self):
+    def __get_api_resource(self):
         c = self.__get_server_config()
+
         m = magento.MagentoAPI(
                 c['hostname'], 
                 c['port'], 
                 c['username'], 
-                c['password'])
+                c['password'],
+                proto=c['scheme'])
 
         return m
 
     @property
+    def api_resource(self):
+        return self.__ar
+
+
+_API_RESOURCES = None
+def _get_api_resources():
+    global _API_RESOURCES
+    if _API_RESOURCES is None:
+        _API_RESOURCES = _ApiResources()
+
+    return _API_RESOURCES
+
+
+class Api(object):
+    def __init__(self):
+        ars = _get_api_resources()
+        self.__ar = ars.api_resource
+
+    @property
     def magento(self):
-        return self.__m
+        return self.__ar
--- ma/config/magento.py
+++ ma/config/magento.py
@@ -3,10 +3,11 @@ import logging
 
 _LOGGER = logging.getLogger(__name__)
 
+SCHEME = os.environ.get('MAGENTO_SCHEME', 'https')
 USERNAME = os.environ['MAGENTO_USERNAME']
 PASSWORD = os.environ['MAGENTO_PASSWORD']
 HOSTNAME = os.environ['MAGENTO_HOSTNAME']
-PORT = int(os.environ.get('MAGENTO_PORT', '80'))
+PORT = int(os.environ.get('MAGENTO_PORT', '443'))
 
-_LOGGER.debug("CONFIG: U=[%s] P=[%s] H=[%s] P=(%d)",
-              USERNAME, PASSWORD, HOSTNAME, PORT)
+_LOGGER.debug("CONFIG: S=[%s] U=[%s] P=[%s] H=[%s] P=(%d)",
+              SCHEME, USERNAME, PASSWORD, HOSTNAME, PORT)
